67e5ef864b0027bd09e5f10501f47bf0ce155f78
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-23 10:39:21 -0300

    Aplicacao das streams em um modulo http

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,25 @@
+import { Readable } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+  index = 1;
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i)); // Buffer aceita só string, não numbers
+
+        this.push(buf);
+      }
+    }, 500); // Este timeout emula o que acontece com uma Stream
+  }
+}
+
+// ENVIA DADOS AOS POUCOS PARA O HTTP SERVER. É A BASE DO PODER DA ARQUITETURA DO NODE (CANAL DE ENVIO DE DADOS QUE FICA ABERTO)
+fetch('http://localhost:3335', {
+  method: 'POST',
+  body: new OneToHundredStream(),
+  duplex: 'half',
+});
