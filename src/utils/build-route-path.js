2a5301524a7ee4c828b22f792e386cabf4fa8d1a
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-28 10:37:35 -0300

    route e query params e route com RegExp

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,8 @@
+// Test Regex...
+// /users/:id
+
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-Z]+)/g;
+
+  console.log(Array.from(path.matchAll(routeParametersRegex)));
+}
